2007442be65e6114378001b7103ec7e98e3570dd
ANWAR K V <zaidanwar962@gmail.com>
2021-01-02 11:58:10 +0530

    settings changed

--- mypersonalsite/urls.py
+++ mypersonalsite/urls.py
@@ -21,5 +21,8 @@ from django.views.static import serve
 urlpatterns = [
     path('', include('shameem.urls')),
     path('admin/', admin.site.urls),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
 
 ]
